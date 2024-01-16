ccbe4c368dcc4d2cbe085657e74ad35a78813547
Abubaker Siddique <Abubakersiddique761@gmail.com>
2024-01-16 22:30:05 +0530

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
 
-export default function Home() {
+import { Button, Card, CardFooter, CardHeader, Chip, Image, Link, Textarea } from '@nextui-org/react';
+import { LightningBoltIcon, Pencil2Icon, PlusIcon, ReaderIcon, TrashIcon } from '@radix-ui/react-icons';
+import { useEffect, useState } from 'react';
+import { remark } from 'remark';
+import html from 'remark-html';
+
+interface Note {
+  id: number;
+  content: string;
+  htmlContent: string;
+}
+
+const Home: React.FC = () => {
+  if (!global?.window) return null;
+
+  const [newNote, setNewNote] = useState<string>('');
+  const [notes, setNotes] = useState<Note[]>(window.localStorage.getItem('all-complex-notes') != undefined ? JSON.parse(String(window.localStorage.getItem('all-complex-notes'))) : []);
+  const [editingNoteId, setEditingNoteId] = useState<number | null>(null);
+
+  const handleCreateNote = async () => {
+    if (newNote.trim() !== '') {
+      if (editingNoteId !== null) {
+        // Update existing note
+        const code = await remark().use(html).process(String(newNote))
+        setNotes((prevNotes) =>
+          prevNotes.map((note) =>
+            note.id === editingNoteId ? { ...note, content: newNote, htmlContent: code.toString() } : note
+          )
+        );
+        setEditingNoteId(null);
+      } else {
+        const code = await remark().use(html).process(String(newNote))
+        // Create new note
+        setNotes((prevNotes) => [
+          ...prevNotes,
+          { id: Date.now(), content: newNote, htmlContent: code.toString() },
+        ]);
+      }
+      setNewNote('');
+      window.localStorage.setItem('all-complex-notes', JSON.stringify([...notes, newNote]))
+    }
+  };
+
+  const handleEdit = (id: number) => {
+    const noteToEdit = notes.find((note) => note.id === id);
+    if (noteToEdit) {
+      setNewNote(noteToEdit.content);
+      setEditingNoteId(id);
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+    setEditingNoteId(null);
+  };
+
+  // Update local storage whenever tasks state changes
+  useEffect(() => {
+    localStorage.setItem('all-complex-notes', JSON.stringify(notes));
+  }, [notes]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className='w-screen h-screen flex items-center justify-center py-2 md:p-10 2xl:p-20 overflow-x-hidden text-sm md:text-base'>
+      <div className='p-2 pb-8 w-full xl:4/5 2xl:w-3/5 h-full'>
+        <Card className="w-full">
+          <CardHeader className="flex gap-3">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              alt="Complex Notes Logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/91818906?s=400&u=6e1018587b8c64e66afb1456061b5638eab86fe0&v=4"
+              width={40}
             />
-          </a>
+            <div className="flex flex-col">
+              <p className="text-md">Complex Notes</p>
+              <p className="text-xs md:text-small text-default-500">
+                Made by
+                <Link
+                  className='mx-1 text-xs md:text-small'
+                  isExternal
+                  showAnchorIcon
+                  href="https://github.com/Abubakersiddique761"
+                >
+                  Abubakersiddique761
+                </Link></p>
+            </div>
+          </CardHeader>
+        </Card>
+        <div className='lg:columns-2 gap-2 mt-2'>
+          {notes.map((note) => (
+            <Card key={note.id} className='mb-2'>
+              <div className='p-4 flex items-start justify-between space-x-2'>
+                <div className='overflow-auto w-full prose prose-invert prose-img:rounded-xl' dangerouslySetInnerHTML={{ __html: note.htmlContent }}></div>
+                <div className='flex items-center justify-center flex-col space-y-2'>
+                  <Button color='warning' isIconOnly onClick={() => handleEdit(note.id)}>
+                    <Pencil2Icon />
+                  </Button>
+                  <Button color='danger' isIconOnly onClick={() => handleDelete(note.id)}>
+                    <TrashIcon />
+                  </Button>
+                </div>
+              </div>
+            </Card>
+          ))}
+          {
+            notes.length == 0 ?
+              <Chip color="danger" variant="flat">No note is created.</Chip>
+              :
+              <></>
+          }
+        </div>
+        <div className='mt-2'>
+          <Textarea
+            label="Note"
+            placeholder="Write..."
+            className="max-w-full"
+            description="You can use markdown syntax to make rich edits."
+            value={newNote}
+            onValueChange={(value) => setNewNote(value)}
+            maxRows={25}
+          />
+        </div>
+        <div className='mt-2 flex items-center justify-end space-x-2 pb-8'>
+          {
+            editingNoteId !== null ?
+              <Button color='warning' aria-label="Update" className='ml-2' onClick={handleCreateNote} >
+                <LightningBoltIcon /> Update
+              </Button>
+              :
+              <Button color='primary' aria-label="Add" className='ml-2' onClick={handleCreateNote} >
+                <PlusIcon /> Create Note
+              </Button>
+          }
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
